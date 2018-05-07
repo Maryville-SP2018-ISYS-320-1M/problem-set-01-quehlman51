97c746672108537a201492d02c57dabf2a6c1a9a
quehlman51 <33034326+quehlman51@users.noreply.github.com>
2018-05-07 01:38:56 -0500

    Created methods

--- src/P2_FightSong.java
+++ src/P2_FightSong.java
@@ -1,11 +1,29 @@
 /*
   	ISYS 320
-  	Name(s):
+  	Name(s): Anthony Quehl
   	Date: 
 */
 
 public class P2_FightSong {
 	public static void main(String[] args) {
-
+		method1();
+		method2();
+		method2();
+		method1();
+		
 	}
+			public static void method1() {
+				System.out.println("Go, team, go!");
+				System.out.println("You can do it.");
+				System.out.println();
+			}
+			public static void method2() {
+				System.out.println("Go, team, go!");
+				System.out.println("You can do it.");
+				System.out.println("You're the best,");
+				System.out.println("In the West.");
+				System.out.println("Go, team, go!");
+				System.out.println("You can do it.");
+				System.out.println();
+			}
 }

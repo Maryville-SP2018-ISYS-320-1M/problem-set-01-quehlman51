a2f50f6f28c01790166a4070e3ba160b671d66d3
quehlman51 <33034326+quehlman51@users.noreply.github.com>
2018-05-07 01:43:44 -0500

    Created expression

--- src/P3_YourExpression.java
+++ src/P3_YourExpression.java
@@ -1,12 +1,13 @@
 /*
   	ISYS 320
-  	Name(s):
+  	Name(s): Anthony Quehl
   	Date: 
 */
 public class P3_YourExpression {
 
 	public static void main(String[] args) {
-
+		int expression = ((2 * 3) * 4);
+		System.out.println(expression);
 	}
 
 }

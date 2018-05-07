e290bcd6c82c46ca4c6bef0749a22b0556182650
quehlman51 <33034326+quehlman51@users.noreply.github.com>
2018-05-07 01:48:33 -0500

    Created and initialized values and computation

--- src/P4_Position.java
+++ src/P4_Position.java
@@ -7,7 +7,12 @@
 public class P4_Position {
 
 	public static void main(String[] args) {
-
+		double s0 = 2;
+		double v0 = 5;
+		double t = 10;
+		double a = 12;
+		double s = s0 + (v0 * t) + (.5 * (a * (t * t))); 
+		System.out.println(s);
 	}
 
 }

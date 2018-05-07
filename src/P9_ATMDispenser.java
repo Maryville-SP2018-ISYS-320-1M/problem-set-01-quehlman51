364af16d7f4e69be7e99467348452c5df8ffaf22
quehlman51 <33034326+quehlman51@users.noreply.github.com>
2018-05-07 01:52:56 -0500

    Created program

--- src/P9_ATMDispenser.java
+++ src/P9_ATMDispenser.java
@@ -1,12 +1,16 @@
 /*
 	ISYS 320
-	Name(s):
+	Name(s): Anthony Quehl
 	Date: 
 */
 
 public class P9_ATMDispenser {
 
 	public static void main(String[] args) {
+		int withdraw = 25;
+		int tens = withdraw/10;
+		int ones = 5;
+		System.out.println("Withdrawal of $" + withdraw + " dollars would produce " + tens + " $10 dollar bills and " + ones +" $1 bills");
 		
 	}
 

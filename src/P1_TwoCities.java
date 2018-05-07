db373b843a7b694adbea2d712b01ae21aa13f63f
quehlman51 <33034326+quehlman51@users.noreply.github.com>
2018-05-07 01:31:41 -0500

    Created program

--- src/P1_TwoCities.java
+++ src/P1_TwoCities.java
@@ -1,13 +1,16 @@
 /*
   	ISYS 320
-  	Name(s):
+  	Name(s): Anthony Quehl
   	Date: 
 */
 
 public class P1_TwoCities {
 
 	public static void main(String[] args) {
-
+		System.out.println("It was the best of times,");
+		System.out.println("It was the \"worst\" of times,");
+		System.out.println("It was the age of wisdom\\foolishness,");
+		System.out.println("etc.");
 	}
 }
 
